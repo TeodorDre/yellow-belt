1a185aeeeef334db40863798082132cf4a9d7315
TeodorDre <swen295@gmail.com>
2022-02-15 16:45:22 +0300

    feat(core): add region database

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "iostream"
 #include "vector"
+#include "map"
+#include "tuple"
 
 using namespace std;
 
 int main() {
-
+    return 0;
 }
--- objectives/region-database/region-database.cpp
+++ objectives/region-database/region-database.cpp
@@ -1,4 +1,20 @@
-//
-// Created by Andrew Slesarenko on 09.02.2022.
-//
+#include "iostream"
+#include "vector"
+#include "map"
+#include "tuple"
 
+using namespace std;
+
+bool operator<(const Region &lhs, const Region &rhs) {
+    return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+    tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const vector<Region> &regions) {
+    int result = 0;
+    map<Region, int> repetition_count;
+    for (const Region &region : regions) {
+        result = max(result, ++repetition_count[region]);
+    }
+    return result;
+}
